0479556c58d14d1888ae60b0bbb5259188e82920
kenedizo <mwangi260@gmail.com>
2022-09-22 12:30:34 -0700

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,20 +7,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
+	int len = 0, i;
 
-	while (*(dest + count) != '\0')
+	while (dest[len])
+		len++;
+	for (i = 0 ; src[i] != 0 ; i++)
 	{
-		count++;
+		dest[len] = src[i];
+		len += l;
 	}
-
-	while (count2 >= 0)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
+	dest[len] = '\0';
 	return (dest);
 }
